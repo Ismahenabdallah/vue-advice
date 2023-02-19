478adcc453a69a00af1c7adc0db5fd1659e33674
Ismahenabdallah <abdallah.ismahen99@gmail.com>
2023-02-19 12:49:38 +0100

    first-project

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <page-header :title="title" :text="text"> </page-header>
+  <page-form></page-form>
+</template>
+
+<script>
+import PageHeader from "./components/PageHeader.vue";
+import PageForm from "./components/PageForm.vue";
+import quotes from "../data.json";
+export default {
+  name: "App",
+  components: {
+    PageHeader,
+    PageForm,
+  },
+  provide() {
+    return {
+      dataJson: this.quotes,
+    };
+  },
+  data() {
+    return {
+      quotes,
+    };
+  },
+};
+</script>
+
+<style>
+@import "./assets/css/style.css";
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/PageForm.vue
@@ -0,0 +1,82 @@
+<template>
+  <form class="form">
+    <fieldset class="form-fieldset">
+      <legend class="form-legend">Choose a number</legend>
+      <label class="form-label">Luck Number</label>
+      <input class="form-input" type="number" v-model="userNumber" />
+    </fieldset>
+    <fieldset class="form-fieldset">
+      <legend class="form-legend">Text Color</legend>
+      <label class="form-label">Color</label>
+      <input class="form-input" type="color" v-model="userColor" />
+    </fieldset>
+    <form-result :color="userColor" :advice="getAdvice"></form-result>
+  </form>
+</template>
+
+<script>
+import FormResult from "./FormResult.vue";
+export default {
+  name: "PageForm",
+  components: {
+    FormResult,
+  },
+  inject: ["dataJson"],
+
+  data() {
+    return {
+      userNumber: 5,
+      userColor: "#000000",
+    };
+  },
+  computed: {
+    getAdvice() {
+      const advice = this.dataJson.find(
+        (q, index) => index === this.userNumber
+      );
+
+      if (!advice) return "unlucky enough, no advice for you";
+      return advice.quoteText;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.form {
+  margin: 5rem auto;
+  max-width: 70%;
+}
+
+.form-fieldset {
+  margin: 2rem 0;
+  padding: 2rem;
+  border-radius: 10px;
+  background: linear-gradient(80deg, #e5e5e5, lightblue);
+}
+
+.form-legend {
+  display: block;
+  padding: 1rem;
+  font-size: 1rem;
+  font-weight: bold;
+  background: linear-gradient(80deg, yellow, lightgreen);
+  border-radius: 10px;
+}
+
+.form-label {
+  display: block;
+  padding: 1rem;
+  font-size: 2rem;
+  font-weight: bold;
+}
+
+.form-input {
+  outline: none;
+  border: none;
+  font-size: 2rem;
+  color: tomato;
+  text-align: center;
+  max-width: 30%;
+}
+</style>
